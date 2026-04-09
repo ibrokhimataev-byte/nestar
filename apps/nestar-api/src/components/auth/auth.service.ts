4c76187aa61ee9531d4d4503f48e829cc0ec1042
Leo <ibrokhimataev@gmail.com>
2026-04-09 13:52:17 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { Member } from '../../libs/dto/member/member';
+import { JwtService } from '@nestjs/jwt';
+import { T } from '../../libs/types.ts/common';
 
 @Injectable()
 export class AuthService {
+	constructor(private jwtService: JwtService) {}
+
 	public async hashPassword(password: string): Promise<string> {
 		const salt = await bcrypt.genSalt();
 		return await bcrypt.hash(password, salt);
 	}
 
-    public async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
-        return await bcrypt.compare(password, hashedPassword);
-    }
+	public async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+		return await bcrypt.compare(password, hashedPassword);
+	}
+
+	public async createToken(member: Member): Promise<string> {
+		const payload: T = {};
+		Object.keys(member['_doc'] ? member['_doc'] : member).map((ele) => {
+			payload[`${ele}`] = member[`${ele}`];
+		});
+		delete payload.memberPassword; // Remove password from payload for security
+		console.log('Payload for token:', payload);
+
+		return await this.jwtService.signAsync(payload);
+	}
+
+	public async verifyToken(token: string): Promise<Member> {
+		const member = await this.jwtService.verifyAsync(token);
+		return member;
+	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -20,6 +20,7 @@ export class MemberService {
 		try {
 			const result = await this.memberModel.create(input);
 			// TODO: Authentication via TOKEN
+			result.accessToken = await this.authService.createToken(result);
 			return result;
 		} catch (err) {
 			console.log('Error, service.model:', err);
@@ -43,6 +44,7 @@ export class MemberService {
 		// TODO: compare password
 		const isMatch = await this.authService.comparePassword(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		response.accessToken = await this.authService.createToken(response); // Generate token and add to response
 
 		return response;
 	}
