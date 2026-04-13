c0363a04c9f1b5ac61e4c2293a04261ae885e563
Leo <ibrokhimataev@gmail.com>
2026-04-14 01:44:30 +0900

    feat: develop getAllMembersByAdmin andupdateMemberByAdmin API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { AgentInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -20,13 +20,13 @@ export class MemberResolver {
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
 		console.log('Mutation signup');
-		return this.memberService.signup(input);
+		return await this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		console.log('Mutation login');
-		return this.memberService.login(input);
+		return await this.memberService.login(input);
 	}
 
 	@UseGuards(AuthGuard)
@@ -37,7 +37,7 @@ export class MemberResolver {
 	): Promise<Member> {
 		console.log('Mutation: updateMember');
 		delete input._id; //input ichida kelgan memberid kerak emas sababi uni @AuthMember("_id") shu orqali qolga allaqachon kiritganmiz
-		return this.memberService.updateMember(memberId, input);
+		return await this.memberService.updateMember(memberId, input);
 	}
 
 	@UseGuards(AuthGuard)
@@ -62,29 +62,35 @@ export class MemberResolver {
 	 public async getMember(@Args("memberId") input: string, @AuthMember('_id') memberId: mongoose.ObjectId): Promise<Member> {
 		console.log('Mutation: getMember');
 		const targetId = shapeIntoMongoObjectId(input);
-		 return this.memberService.getMember(memberId, targetId);
+		 return await this.memberService.getMember(memberId, targetId);
 	}
 
 	 @UseGuards(WithoutGuard) //agentlar royxatini butun malumotlar bilan birgalikda olib beradi
     @Query(() => Members)
     public async getAgents(@Args("input") input: AgentInquiry, @AuthMember('_id') memberId: mongoose.ObjectId): Promise<Members>{
         console.log("Query getAgents")
-        return this.memberService.getAgents(memberId, input);
+        return await this.memberService.getAgents(memberId, input);
     }
 	/** ADMIN **/
 
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
-	@Mutation(() => String)
-	public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<string> {
-		console.log('authMember.memberType', authMember.memberType);
-		return this.memberService.getAllMembersByAdmin();
+	@Query(() => Members)
+    public async getAllMembersByAdmin(@Args("input") input: MembersInquiry): Promise<Members> {
+           console.log("Mutation: getAllMembersByAdmin");
+        return await this.memberService.getAllMembersByAdmin(input);
+
 	}
 
 	// Authorization: Admin
-	@Mutation(() => String)
-	public async updateMemberByAdmin(): Promise<string> {
-		console.log('Mutation updateMemberByAdmin');
-		return this.memberService.updateMemberByAdmin();
-	}
+	 @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => Member) 
+    public async updateMemberByAdmin(@Args("input") input: MemberUpdate): Promise<Member> {
+        console.log("Mutation: updateMember");
+        return await this.memberService.updateMemberByAdmin(input);
+    }
+
+
 }
+
