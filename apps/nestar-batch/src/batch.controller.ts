e8be16bc075308f4d52dc50c4b8d35e746235027
Leo <ibrokhimataev@gmail.com>
2026-04-28 07:11:55 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedules logic

---
+++ apps/nestar-batch/src/batch.controller.ts
@@ -0,0 +1,65 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { Cron, Timeout } from '@nestjs/schedule';
+import { BatchService } from './batch.service';
+
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
+
+@Controller()
+export class BatchController {
+  private logger: Logger = new Logger('BatchController');
+
+  constructor(private readonly batchService: BatchService) {}
+
+@Timeout(1000) //Bu decorator har belgilangan vaqtda biror mantiqni bir marta ishga tushiradi
+  handleTimeout() {
+    this.logger.debug('BATCH SERVER READY');
+  }
+//                  JobSchedulers
+
+@Cron('00 00 01 * * *', { name: 'BATCH_ROLLBACK' })
+  public async batchRollback() {
+    try {
+      this.logger['context'] = BATCH_ROLLBACK;
+      this.logger.debug('EXECUTED!');
+      await this.batchService.batchRollback();
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+  @Cron('20 00 01 * * *', { name: 'BATCH_TOP_PROPERTIES' })
+  public async batchTopProperties() {
+    try {
+      this.logger['context'] = BATCH_TOP_PROPERTIES;
+      this.logger.debug('EXECUTED!');
+      await this.batchService.batchTopProperties();
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+  @Cron('40 00 01 * * *', { name: 'BATCH_TOP_AGENTS' })
+  public async batchTopAgents() {
+    try {
+     
+    } catch (err) {
+      this.logger.error(err);
+    }
+    this.logger['context'] = BATCH_TOP_AGENTS;
+    this.logger.debug('EXECUTED!');
+     await this.batchService.batchTopAgents();
+  }
+
+  /**
+  @Interval(1000) //Bu decorator har belgilangan vaqtda biror mantiqni doimiy ishga tushirib turadi
+  handleInterval() {
+  this.logger.debug('INTERVAL TEST');
+  }
+   */
+ 
+
+  @Get()
+  getHello(): string {
+    return this.batchService.getHello();
+  }
+}
