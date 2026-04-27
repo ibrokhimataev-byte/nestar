04e129118e76cf6e51e78fe4ffac551dac4891f7
Leo <ibrokhimataev@gmail.com>
2026-04-28 07:32:04 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
-import { BatchController } from './batch.controller';
-import { BatchService } from './batch.service';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BatchController as BatchController } from './batch.controller';
+import { BatchService } from './batch.service';
+import { ConfigModule } from '@nestjs/config'; //.env ichidagilarni import qilish imkonini beruvchi package
+import { DatabaseModule } from './database/database.module';
+
 
 import { MongooseModule } from '@nestjs/mongoose';
 import PropertySchema from '../../nestar-api/src/schemas/Property.model';
 import MemberSchema from '../../nestar-api/src/schemas/Member.model';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(), 
-    DatabaseModule, 
-    ScheduleModule.forRoot(),
-    MongooseModule.forFeature([{name:'Property', schema:PropertySchema}]),
-    MongooseModule.forFeature([{name:'Member', schema:MemberSchema}]),
-  ],
-  controllers: [BatchController],
-  providers: [BatchService],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
+	controllers: [BatchController],
+	providers: [BatchService],
 })
 export class BatchModule {}
