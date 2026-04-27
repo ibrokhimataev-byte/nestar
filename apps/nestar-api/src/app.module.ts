0ada0dd282e9464ebe03e153d5f683d3b1472cbd
Leo <ibrokhimataev@gmail.com>
2026-04-28 06:28:35 +0900

    create websocket connection process

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -1,36 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from "@nestjs/config"; //.env ichidagilarni import qilish imkonini beruvchi package
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver } from "@nestjs/apollo";
+import { ConfigModule } from '@nestjs/config'; //.env ichidagilarni import qilish imkonini beruvchi package
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types.ts/common';
 import { PropertyModule } from './components/property/property.module';
-import { PropertyService } from './components/property/property.service';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(), 
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      playground: true,
-      uploads: false,
-      autoSchemaFile: true,
-      formatError: (error: T) => { //=> graphqldaa sodir bolgan errorlarni olib beradi 
-        const graphQlFormattedError ={
-          code: error?.extensions.code,
-          message: error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
-        };
-        console.log("graphQl Global Error:", graphQlFormattedError);
-        return graphQlFormattedError; //server osilib qolmasligi uchun graphQlFormattedError ni return qildik
-      }
-    }), 
-    ComponentsModule, //components moduleda member, property va h.k modullerni tashkillashtiramiz.ularni components module umumlasjtiradi.components ni esa asosiy app.modulega import qilib oldik
-    DatabaseModule, PropertyModule], //Databasega ulanish mantigi yozilgan module.Uni alohida tashkillashtirdik sababi loyiha ishga tushganda databasega connect 1 marta amalga oshadi va yakunlanadi
-  controllers: [AppController],
-  providers: [AppService, AppResolver],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+			formatError: (error: T) => {
+				//=> graphqldaa sodir bolgan errorlarni olib beradi
+				const graphQlFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('graphQl Global Error:', graphQlFormattedError);
+				return graphQlFormattedError; //server osilib qolmasligi uchun graphQlFormattedError ni return qildik
+			},
+		}),
+		ComponentsModule, //components moduleda member, property va h.k modullerni tashkillashtiramiz.ularni components module umumlasjtiradi.components ni esa asosiy app.modulega import qilib oldik
+		DatabaseModule,
+		PropertyModule,
+		SocketModule,
+	], //Databasega ulanish mantigi yozilgan module.Uni alohida tashkillashtirdik sababi loyiha ishga tushganda databasega connect 1 marta amalga oshadi va yakunlanadi
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -3,16 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
-import * as express from 'express'; 
+import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());
-  app.enableCors({origin: true, credentials: true});
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new LoggingInterceptor());
+	app.enableCors({ origin: true, credentials: true });
 
-  app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-  app.use('/uploads', express.static('./uploads')); // 10MB limit for file uploads
-  await app.listen(process.env.PORT_API ?? 3007);
+	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+	app.use('/uploads', express.static('./uploads')); // 10MB limit for file uploads
+	app.useWebSocketAdapter(new WsAdapter(app));
+
+	await app.listen(process.env.PORT_API ?? 3007);
 }
 bootstrap();
